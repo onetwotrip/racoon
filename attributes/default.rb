55fc75a77d4de0c56d4ac14955a5b48aa2a2e46b
Tony Nyurkin <ptqa.mail@gmail.com>
2015-01-23 17:02:59 +0300

    update

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+default['racoon']['pre_shared_key'] 	= '/etc/racoon/psk.txt'
+default['racoon']['certificate'] 	= '/etc/racoon/certs'
+default['racoon']['listen_addr'] 	= '0.0.0.0'
+default['racoon']['listen_port'] 	= 500
+default['racoon']['lifetime'] 		= '1 hour'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,3 +2,10 @@
 # Recipe:: default
 
 apt_package "racoon"
+
+directory "/etc/racoon/racoon.d/" do
+  owner 'root'
+  group 'root'
+  mode '0644'
+  action :create
+end
