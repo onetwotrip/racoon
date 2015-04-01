c14a01458a7a72fda6dbe4eaff887597377c25ab
Tony Nyurkin <ptqa.mail@gmail.com>
2015-04-01 10:56:30 +0300

    new version

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,3 +2,8 @@
 # Recipe:: default
 
 include_recipe 'runit' if node['racoon']['init_style'] == 'runit'
+
+racoon node['racoon']['remote_ip'] do
+  pre_shared_key node['racoon']['key']
+  encryption_algorithms node['racoon']['encryption']
+end if node['racoon']['remote_ip']
